0e1443dd19525090e9941f4664920a194c79f023
Wildy Estephan <westephan50@gmail.com>
2020-05-22 18:36:59 -0400

    Update __manifest__.py

--- maintenance_cp/__manifest__.py
+++ maintenance_cp/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_invoicing', 'stock', 'hr', 'account_asset'],
+    'depends': ['mail', 'account_invoicing', 'stock', 'hr', 'account_asset'],
 
     # always loaded
     'data': [
