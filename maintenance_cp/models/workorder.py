d081c9bc7a2287062cd73fdec4fe3db981cab549
Wildy Estephan <westephan50@gmail.com>
2020-05-22 22:55:21 -0400

    Update workorder.py

--- maintenance_cp/models/workorder.py
+++ maintenance_cp/models/workorder.py
@@ -136,8 +136,8 @@ class WorkOrder(models.Model):
     def send_request(self):
         self.state = 'send'
 
-        self.sudo().add_followers()
-        self.sudo().send_message()
+        # self.sudo().add_followers()
+        # self.sudo().send_message()
 
 
     def approve_this(self):
