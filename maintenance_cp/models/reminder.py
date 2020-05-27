5a95db7a3cb022816d6cc2333f09d098c7a5321e
Wildy Estephan <westephan50@gmail.com>
2020-05-27 10:01:16 -0400

    Update reminder.py

--- maintenance_cp/models/reminder.py
+++ maintenance_cp/models/reminder.py
@@ -68,7 +68,7 @@ class ReminderTask(models.Model):
         # raise exceptions.UserError((today))
 
         reminders = self.search(
-            ('nextcall', '=', today)
+            ('nextcall', '=', datetime.strptime(today, '%Y-%m-%d %H:%M:%S'))
         )
 
         for reminder in reminders:
