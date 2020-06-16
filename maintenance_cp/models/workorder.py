081a1601fe0eb5e1dc21abf87ff0815d9b187a1b
Wildy Estephan <westephan50@gmail.com>
2020-06-16 09:46:28 -0400

    Update workorder.py

--- maintenance_cp/models/workorder.py
+++ maintenance_cp/models/workorder.py
@@ -320,8 +320,8 @@ class PlannedParts(models.Model):
     product_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'),
                                required=True)
     estimated_cost = fields.Float(
-        string='Estimated Cost',
-        required=True)
+        string='Estimated Unit Cost',
+        required=False)
     state = fields.Selection(string="State",
                              selection=[('stock', 'On Stock'),
                                         ('unavailable', 'Unavailable'),
